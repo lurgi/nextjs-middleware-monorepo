0a90741f3be61bb3bea1d5117ca4dc93ca71d0e3
Jeongwoo Park <wahoo9040@naver.com>
2025-01-24 13:59:43 +0900

    build: 라이브러리 build 설정

--- packages/next-middleware-enhancer/vite.config.ts
+++ packages/next-middleware-enhancer/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    dts({
+      entryRoot: "src", // 타입 엔트리 지정
+      outDir: "dist", // dist에 index.d.ts 생성
+      insertTypesEntry: true, // package.json에 자동으로 `"types"` 필드 추가
+      rollupTypes: true, // 타입을 하나로 번들링
+      tsconfigPath: "tsconfig.build.json",
+    }),
+  ],
   build: {
     lib: {
       entry: "src/index.ts",
