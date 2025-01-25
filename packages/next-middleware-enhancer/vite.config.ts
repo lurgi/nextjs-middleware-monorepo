0cb635368050dac68f0454994972782a08283eca
Jeongwoo Park <wahoo9040@naver.com>
2025-01-25 16:01:09 +0900

    chore: 불필요한 코드 삭제
    squash

--- packages/next-middleware-enhancer/vite.config.ts
+++ packages/next-middleware-enhancer/vite.config.ts
@@ -1,26 +1,25 @@
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import dts from "vite-plugin-dts";
+import fs from "fs";
+import path from "path";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
     dts({
       entryRoot: "src", // 타입 엔트리 지정
-      outDir: "dist", // dist에 index.d.ts 생성
-      insertTypesEntry: true, // package.json에 자동으로 `"types"` 필드 추가
-      rollupTypes: true, // 타입을 하나로 번들링
-      tsconfigPath: "tsconfig.build.json",
+      outDir: "dist",
+      rollupTypes: true,
     }),
   ],
   build: {
     lib: {
       entry: "src/index.ts",
       name: "NextMiddlewareEnhancer",
+      formats: ["es", "cjs"],
       fileName: (format) => `next-middleware-enhancer.${format}.js`,
     },
     rollupOptions: {
-      external: ["next"],
+      external: ["next", "next/server"],
     },
   },
 });
